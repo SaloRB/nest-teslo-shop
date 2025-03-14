6df83e91b4ff23567e92b870c773ad6ba5477aeb
Salomon Romano <salo_romano@yahoo.com>
2025-03-14 12:05:16 -0600

    refactor(products): change price column type to float and enhance create product method with error handling

--- src/products/products.service.ts
+++ src/products/products.service.ts
@@ -1,11 +1,34 @@
-import { Injectable } from '@nestjs/common';
+import {
+  BadRequestException,
+  Injectable,
+  InternalServerErrorException,
+  Logger,
+} from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
+
 import { CreateProductDto } from './dto/create-product.dto';
 import { UpdateProductDto } from './dto/update-product.dto';
 
+import { Product } from './entities/product.entity';
+
 @Injectable()
 export class ProductsService {
-  create(createProductDto: CreateProductDto) {
-    return 'This action adds a new product';
+  private readonly logger = new Logger('ProductsService');
+
+  constructor(
+    @InjectRepository(Product)
+    private readonly productsRepository: Repository<Product>,
+  ) {}
+
+  async create(createProductDto: CreateProductDto) {
+    try {
+      const newProduct = this.productsRepository.create(createProductDto);
+      await this.productsRepository.save(newProduct);
+      return newProduct;
+    } catch (error) {
+      this.handleDbExceptions(error, 'Error creating product');
+    }
   }
 
   findAll() {
@@ -17,10 +40,24 @@ export class ProductsService {
   }
 
   update(id: number, updateProductDto: UpdateProductDto) {
+    console.log(updateProductDto);
     return `This action updates a #${id} product`;
   }
 
   remove(id: number) {
     return `This action removes a #${id} product`;
+  }
+
+  private handleDbExceptions(error: any, message?: string) {
+    if (
+      error instanceof Error &&
+      'code' in error &&
+      'detail' in error &&
+      error.code === '23505'
+    ) {
+      throw new BadRequestException(error.detail);
+    }
+    this.logger.error(error);
+    throw new InternalServerErrorException(message || 'Unexpected error');
   }
 }
