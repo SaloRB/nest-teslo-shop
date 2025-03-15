2141a3688c76fe8ca37591906db080035716bd19
Salomon Romano <salo_romano@yahoo.com>
2025-03-15 10:29:27 -0600

    feat(products): establish relationship between Product and ProductImage entities

--- src/products/entities/product.entity.ts
+++ src/products/entities/product.entity.ts
@@ -3,8 +3,11 @@ import {
   BeforeUpdate,
   Column,
   Entity,
+  OneToMany,
   PrimaryGeneratedColumn,
 } from 'typeorm';
+
+import { ProductImage } from './';
 
 @Entity()
 export class Product {
@@ -51,7 +54,10 @@ export class Product {
   })
   tags: string[];
 
-  // images
+  @OneToMany(() => ProductImage, (productImage) => productImage.product, {
+    cascade: true,
+  })
+  images?: ProductImage[];
 
   @BeforeInsert()
   @BeforeUpdate()
