29891f7d2ab5f42440b852a00e1a4d6246afdd63
Salomon Romano <salo_romano@yahoo.com>
2025-03-15 21:55:49 -0600

    feat(seed): implement seed module with controller and service for product seeding

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,6 +4,7 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 
 import { CommonModule } from './common/common.module';
 import { ProductsModule } from './products/products.module';
+import { SeedModule } from './seed/seed.module';
 
 @Module({
   imports: [
@@ -20,6 +21,7 @@ import { ProductsModule } from './products/products.module';
     }),
     ProductsModule,
     CommonModule,
+    SeedModule,
   ],
 })
 export class AppModule {}
