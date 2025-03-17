9fd3b716974e56f92777bfa0a990b67dd294e743
Salomon Romano <salo_romano@yahoo.com>
2025-03-16 20:05:02 -0600

    feat(files): add endpoint to retrieve static product images with error handling

--- src/files/files.controller.ts
+++ src/files/files.controller.ts
@@ -1,17 +1,35 @@
 import {
   BadRequestException,
   Controller,
+  Get,
+  Param,
   Post,
+  Res,
   UploadedFile,
   UseInterceptors,
 } from '@nestjs/common';
 import { FileInterceptor } from '@nestjs/platform-express';
+import { Response } from 'express';
 import { diskStorage } from 'multer';
+
+import { FilesService } from './files.service';
 
 import { fileFilter, fileNamer } from './helpers';
 
 @Controller('files')
 export class FilesController {
+  constructor(private readonly fileService: FilesService) {}
+
+  @Get('product/:imageName')
+  findProductImage(
+    @Res() res: Response,
+    @Param('imageName') imageName: string,
+  ) {
+    const path = this.fileService.getStaticProductImage(imageName);
+
+    res.sendFile(path);
+  }
+
   @Post('product')
   @UseInterceptors(
     FileInterceptor('file', {
@@ -28,8 +46,10 @@ export class FilesController {
       throw new BadRequestException('File is empty');
     }
 
+    const secureUrl = `${file.filename}`;
+
     return {
-      fileName: file.originalname,
+      secureUrl,
     };
   }
 }
