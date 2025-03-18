96bc1665f4b9595105e1d49d878f99f7fb37c542
Salomon Romano <salo_romano@yahoo.com>
2025-03-18 11:32:21 -0600

    feat(auth): implement JWT strategy for user authentication and add JwtPayload interface

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -9,25 +9,26 @@ import { AuthService } from './auth.service';
 
 import { User } from './entities/user.entity';
 
+import { JwtStrategy } from './strategies/jwt.strategy';
+
 @Module({
   controllers: [AuthController],
-  providers: [AuthService],
+  providers: [AuthService, JwtStrategy],
   imports: [
+    ConfigModule,
     TypeOrmModule.forFeature([User]),
     PassportModule.register({ defaultStrategy: 'jwt' }),
     JwtModule.registerAsync({
       imports: [ConfigModule],
       inject: [ConfigService],
-      useFactory: (configService: ConfigService) => {
-        return {
-          secret: configService.get('JWT_SECRET'),
-          signOptions: {
-            expiresIn: '2h',
-          },
-        };
-      },
+      useFactory: (configService: ConfigService) => ({
+        secret: configService.get('JWT_SECRET'),
+        signOptions: {
+          expiresIn: '2h',
+        },
+      }),
     }),
   ],
-  exports: [TypeOrmModule],
+  exports: [TypeOrmModule, JwtStrategy, PassportModule, JwtModule],
 })
 export class AuthModule {}
