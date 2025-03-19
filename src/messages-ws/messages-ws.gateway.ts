b5236f7fbd306381657858c2724bedc449ef3810
Salomon Romano <salo_romano@yahoo.com>
2025-03-19 13:03:54 -0600

    feat(messages): update WebSocket gateway to emit connected clients on connection and disconnection

--- src/messages-ws/messages-ws.gateway.ts
+++ src/messages-ws/messages-ws.gateway.ts
@@ -2,8 +2,9 @@ import {
   OnGatewayConnection,
   OnGatewayDisconnect,
   WebSocketGateway,
+  WebSocketServer,
 } from '@nestjs/websockets';
-import { Socket } from 'socket.io';
+import { Server, Socket } from 'socket.io';
 
 import { MessagesWsService } from './messages-ws.service';
 
@@ -11,17 +12,23 @@ import { MessagesWsService } from './messages-ws.service';
 export class MessagesWsGateway
   implements OnGatewayConnection, OnGatewayDisconnect
 {
+  @WebSocketServer() wss: Server;
+
   constructor(private readonly messagesWsService: MessagesWsService) {}
 
   handleConnection(client: Socket) {
     this.messagesWsService.registerClient(client);
-
-    console.log({
-      'Connected clients': this.messagesWsService.getConnectedClients(),
-    });
+    this.wss.emit(
+      'updated-clients',
+      this.messagesWsService.getConnectedClients(),
+    );
   }
 
   handleDisconnect(client: Socket) {
     this.messagesWsService.removeClient(client.id);
+    this.wss.emit(
+      'updated-clients',
+      this.messagesWsService.getConnectedClients(),
+    );
   }
 }
